402217306455ee62a0ebae8276c9a2ec9fb51be8
johnnyleeli <johnnyleeli@tencent.com>
2023-02-05 21:36:50 +0800

    main seperated

--- src/array_slice_test.go
+++ src/array_slice_test.go
@@ -12,14 +12,42 @@ func TestArray(f *testing.T) {
 	test1 := array[0]
 	testPtr(&array[0])
 	testArray(array)
+	testAppend(array)
+	testAppendV1(&array)
 
-	fmt.Println(test1)
+	fmt.Println("test1", test1)
 
 	for _, t := range array {
 		fmt.Println("array elements : ", t)
 	}
 
+	//test append
+	slis2 := make([]int, 3, 5)
+	//slis2[3] = 10 index 错误
+	fmt.Printf("slis2 addr %p\n", slis2)
+	slis2 = append(slis2, 10)            //enough space
+	fmt.Printf("slis2 addr %p\n", slis2) //the same as above
+	fmt.Println("slis2", slis2)
+	slis2 = append(slis2, 11, 12)
+	fmt.Println("slis2 len cap", len(slis2), cap(slis2)) //cap * 2
+
+	var slis3 []int
+	// slis3[0] = 10 //nil slice 不可以直接通过下标访问
+	slis3 = append(slis3, 1) //nil slice 可以直接append
+
 	testReSlice()
+}
+
+func testAppend(p []int) {
+	//original slice not changed
+	p = append(p, 10)
+	fmt.Println("test append", p)
+}
+
+func testAppendV1(p *[]int) {
+	//original slice changed
+	*p = append(*p, 10)
+	fmt.Println("test append", *p)
 }
 
 func testArray(p []int) {
@@ -40,12 +68,27 @@ func testPtr(a *int) {
 	*a = 10
 }
 
+/**
+slice := []int{1, 2, 3, 4, 5}
+newSlice := slice[0:3:3]
+第一个值：截取的起始索引
+第二个值：截取的终止索引（不包括该值）
+第三个值：用来计算切片的容量，可以省略，默认和长度一样
+容量 = 第三个值 - 第一个值
+长度 = 第二个值 - 第一个值
+*/
 func testReSlice() {
 	fix := []int{1, 2, 3}
+
+	slisTest := fix[0:1]
+	fmt.Println("slisTest len cap", len(slisTest), cap(slisTest))
+
 	slis := fix[1:3]
+	fmt.Printf("slis address %p, %p \n", fix, slis) //并没有重新分配内存
 	fmt.Println("slis len cap", len(slis), cap(slis))
 	slis[0] = 10
 	//slis[3] = 100 index 错误
+	fmt.Println("fix", fix) //fix[1] 也变成了10
 	fmt.Printf("slis addr %p\n", slis)
 	slis = append(slis, 100) //append完生成新的slice
 	fmt.Printf("slis addr %p\n", slis)
@@ -53,23 +96,12 @@ func testReSlice() {
 	fmt.Println("fix", fix)
 	fmt.Println("slis", slis)
 
-	slis2 := make([]int, 3, 5)
-	//slis2[3] = 10 index 错误
-	fmt.Printf("slis2 addr %p\n", slis2)
-	slis2 = append(slis2, 10)            //enough space
-	fmt.Printf("slis2 addr %p\n", slis2) //the same as above
-	fmt.Println("slis2", slis2)
-
-	var slis3 []int
-	// slis3[0] = 10 //nil slice 不可以直接通过下标访问
-	slis3 = append(slis3, 1) //nil slice 可以直接append
-
 	path := []byte("AAAA/BBBBBBBBB")
 	sepIndex := bytes.IndexByte(path, '/') // 4
 	println(sepIndex)
 
-	dir1 := path[:sepIndex]
-	dir2 := path[sepIndex+1:]
+	dir1 := path[:sepIndex]         // len 4 cap 14
+	dir2 := path[sepIndex+1:]       // len 9 cap 9
 	println("dir1: ", string(dir1)) // AAAA
 	println("dir2: ", string(dir2)) // BBBBBBBBB
 
--- src/str_rune_test.go
+++ src/str_rune_test.go
@@ -2,20 +2,62 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"strconv"
 	"testing"
+	"unicode/utf8"
+	"unsafe"
 )
 
 func TestStr(t *testing.T) {
+	str := "test123456789"
+	str1 := "test" + strconv.Itoa(123456789)
+	str2 := "test123456789"
+	str3 := str2[0:4] //只是把地址偏移量赋给了data指针
+	test := "test"
+
+	fmt.Printf("str %s, %s, %s, %p, %p, %p \n", str, str1, str2, &str, &str1, &str2)
+
+	strptr := (*reflect.StringHeader)(unsafe.Pointer(&str))
+	str1ptr := (*reflect.StringHeader)(unsafe.Pointer(&str1))
+	str2ptr := (*reflect.StringHeader)(unsafe.Pointer(&str2))
+	str3ptr := (*reflect.StringHeader)(unsafe.Pointer(&str3))
+	testptr := (*reflect.StringHeader)(unsafe.Pointer(&test))
+
+	fmt.Println("a ptr:", unsafe.Pointer(strptr.Data))
+	fmt.Println("b ptr:", unsafe.Pointer(str1ptr.Data))
+	fmt.Println("c ptr:", unsafe.Pointer(str2ptr.Data)) // c == d
+	fmt.Println("d ptr:", unsafe.Pointer(str3ptr.Data))
+	fmt.Println("t ptr:", unsafe.Pointer(testptr.Data))
+
+	if str == str1 { //先比较大小，然后比较data指针的值，再逐个byte进行内容对比
+		fmt.Println("str is equal")
+	}
+
+	strByte := []byte(str) //类型转换发生了内容拷贝
+	sliceptr := (*reflect.SliceHeader)(unsafe.Pointer(&strByte))
+	fmt.Printf("str and bytes %s, %s, %p, %p, %p \n", str, strByte, &str, strByte, &strByte)
+	fmt.Println("slice ptr:", unsafe.Pointer(sliceptr.Data))
+	strByte[0] = 'd'
+	str11 := string(strByte) //类型转换再次发生了内容拷贝
+	str11ptr := (*reflect.StringHeader)(unsafe.Pointer(&str11))
+	fmt.Println("str11 ptr:", unsafe.Pointer(str11ptr.Data))
+
+	str = str1 //只是修改了data指针的值
+	fmt.Printf("str %s, %s, %p, %p \n", str, str1, &str, &str1)
+	fmt.Println("a ptr:", unsafe.Pointer(strptr.Data))
+	fmt.Println("b ptr:", unsafe.Pointer(str1ptr.Data))
+
 	testStrTsf()
 }
 
 func testStrTsf() {
 	s := "我是ABC"
 	//s[0] is uint8
-	fmt.Println("this is a utf8 str:", s[0], s[6])
+	fmt.Println("this is a utf8 str:", s[0], s[6], len(s), utf8.RuneCountInString(s)) // 230, 65, 9, 5
 
-	for _, v := range s {
-		//v is int32
+	for _, v := range s { //range的特殊对待，按照rune slice来取值
+		//v 全部都是 int32
 		fmt.Println("this is a range:", v)
 		fmt.Println("this is a utf8 range:", string(v))
 	}
