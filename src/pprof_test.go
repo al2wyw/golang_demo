39f78458937304436e42d513893429dd836765ad
johnnyleeli <johnnyleeli@tencent.com>
2024-01-10 16:09:46 +0800

    pprof

--- src/pprof_test.go
+++ src/pprof_test.go
@@ -5,6 +5,11 @@ import "net/http"
 import "time"
 import _ "net/http/pprof"
 
+// profile 和 trace: profile是cpu时间，trace是io wait时间
+// go tool pprof http://127.0.0.1:8080/debug/pprof/profile?seconds=10
+// curl http://127.0.0.1:8080/debug/pprof/trace?seconds=10 > trace.out
+// go tool pprof -http=127.0.0.1:9888 profile.out
+// go tool trace -http=127.0.0.1:9998 trace.out
 var leak [][1024]byte
 
 func TestProf(t *testing.T) {
